20772389aff10ca397ed2bf5dee5d6059843db96
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-05-27 22:05:05 +0200

    Update leetcode 392, c++ refactor

--- solutions/cpp/isSubsequence/solution.cpp
+++ solutions/cpp/isSubsequence/solution.cpp
@@ -18,7 +18,7 @@ bool isSubsequence(string s, string t) {
   return true;
 
 //  int i = 0, j = 0;
-//  while (i < s.size() and j < t.size()) {
+//  while (i < s.size() && j < t.size()) {
 //    if (s[i] == t[j++]) {
 //      i++;
 //    }
