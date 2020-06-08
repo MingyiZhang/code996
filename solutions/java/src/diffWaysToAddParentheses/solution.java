645d318b56fd090d89bd3d1cdfc4d1428faa6809
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-08 23:24:08 +0200

    Update leetcode 241, java

---
+++ solutions/java/src/diffWaysToAddParentheses/solution.java
@@ -0,0 +1,46 @@
+package diffWaysToAddParentheses;
+
+class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        Map<String, List<Integer>> m = new HashMap<>();
+        return dynamicProgramming(input, m);
+    }
+
+    private List<Integer> dynamicProgramming(String input, Map<String, List<Integer>> m) {
+        if (m.containsKey(input)) {
+            return m.get(input);
+        }
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '+' || input.charAt(i) == '-' || input.charAt(i) == '*') {
+                char op = input.charAt(i);
+                List<Integer> left = dynamicProgramming(input.substring(0, i), m);
+                List<Integer> right = dynamicProgramming(input.substring(i + 1), m);
+                for (Integer x : left) {
+                    for (Integer y : right) {
+                        ret.add(operatorHelper(x, y, op));
+                    }
+                }
+                
+            }
+        }
+        if (ret.isEmpty()) {
+            ret.add(Integer.parseInt(input));
+        }
+        m.put(input, ret);
+        return ret;
+    }
+
+    private Integer operatorHelper(Integer x, Integer y, char op) {
+        switch (op) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y;
+            default:
+                return 0;
+        }
+    }
+}
