1f2334b4b844f67c4547fa6b15854d8c8607d031
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-09 23:19:55 +0200

    Update leetcode 279, java

---
+++ solutions/java/src/perfectSquares/solution.java
@@ -0,0 +1,47 @@
+package perfectSquares;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class solution {
+    private List<Integer> getSquares(int n) {
+        int a = 1;
+        int diff = 3;
+        List<Integer> squares = new ArrayList<>();
+        while (a <= n) {
+            squares.add(a);
+            a += diff;
+            diff += 2;
+        }
+        return squares;
+    }
+
+    public int numSquares(int n) {
+        List<Integer> squares = getSquares(n);
+        int[] visited = new int[n + 1];
+        List<Integer> queue = new ArrayList<>();
+        queue.add(n);
+        visited[n] = 1;
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int qSize = queue.size();
+            count++;
+            while (qSize-- > 0) {
+                int x = queue.get(0);
+                queue.remove(0);
+                for (int i = 0; i < squares.size() && squares.get(i) <= x; i++) {
+                    int next = x - squares.get(i);
+                    if (visited[next] == 1) {
+                        continue;
+                    }
+                    if (next == 0) {
+                        return count;
+                    }
+                    queue.add(next);
+                    visited[next] = 1;
+                }
+            }
+        }
+        return count;
+    }
+}
