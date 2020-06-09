f34654ea9259634ec6e86400979c02d07bdc1af0
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-09 23:25:12 +0200

    Update leetcode 278, java

---
+++ solutions/java/src/firstBadVersion/solution.java
@@ -0,0 +1,16 @@
+package firstBadVersion;
+
+public class solution {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (!isBadVersion(mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
