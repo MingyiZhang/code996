ab766e32efef9f8243d10578185e2091b4358c28
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-09 22:56:51 +0200

    Update leetcode 547, java

---
+++ solutions/java/src/friendCircles/solution.java
@@ -0,0 +1,25 @@
+package friendCircles;
+
+public class solution {
+    public int findCircleNum(int[][] M) {
+        int count = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (M[i][i] == 1) {
+                findFriends(i, M);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void findFriends(int index, int[][] M) {
+        for (int i = 0; i < M.length; i++) {
+            if (index != i && M[index][i] == 1) {
+                M[index][i] = 0;
+                M[i][index] = 0;
+                M[i][i] = 0;
+                findFriends(i, M);
+            }
+        }
+    }
+}
