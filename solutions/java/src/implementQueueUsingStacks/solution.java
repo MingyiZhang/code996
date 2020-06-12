51393f70286f45d8e2acd24897103478e0fb59fb
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-12 23:58:05 +0200

    Add leetcode 232, java

---
+++ solutions/java/src/implementQueueUsingStacks/solution.java
@@ -0,0 +1,49 @@
+package implementQueueUsingStacks;
+
+import java.util.Stack;
+
+public class solution {
+    
+    class MyQueue {
+
+        private Stack<Integer> in = new Stack<>();
+        private Stack<Integer> out = new Stack<>();
+    
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            
+        }
+        
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            in.push(x);
+            
+        }
+        
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            in2out();
+            return out.pop();
+            
+        }
+        
+        /** Get the front element. */
+        public int peek() {
+            in2out();
+            return out.peek();
+        }
+        
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return in.isEmpty() && out.isEmpty();
+        }
+    
+        private void in2out() {
+            if (out.isEmpty()) {
+                while (!in.isEmpty()) {
+                    out.push(in.pop());
+                }
+            }
+        }
+    }
+}
