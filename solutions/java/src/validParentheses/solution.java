d59dea406a42a7f4edaf4b9bab40143ab42b7f72
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-13 23:22:16 +0200

    Add leetcode 20, java

---
+++ solutions/java/src/validParentheses/solution.java
@@ -0,0 +1,28 @@
+package validParentheses;
+
+import java.util.Stack;
+
+public class solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char cOut = stack.pop();
+                boolean b1 = c == ')' && cOut != '(';
+                boolean b2 = c == ']' && cOut != '[';
+                boolean b3 = c == '}' && cOut != '{';
+                if (b1 || b2 || b3) {
+                    return false;
+                }
+
+            }
+        }
+        return stack.isEmpty();
+    }
+}
