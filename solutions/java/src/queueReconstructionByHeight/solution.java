272346f124530cd984cc987d7750fbf77bdb4eec
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-13 23:01:07 +0200

    Update leetcode 406, java

---
+++ solutions/java/src/queueReconstructionByHeight/solution.java
@@ -0,0 +1,25 @@
+package queueReconstructionByHeight;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class solution {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (p1, p2) -> {
+            if (p1[0] == p2[0]) {
+                return p1[1] - p2[1];
+            }
+            return p2[0] - p1[0];
+        });
+        List<int[]> array = new ArrayList<>();
+        for (int i = 0; i < people.length; i++) {
+            array.add(people[i][1], people[i]);       
+        }
+        int[][] ret = new int[people.length][2];
+        for (int i = 0; i < people.length; i++) {
+            ret[i] = array.get(i);
+        }
+        return ret;
+    }
+}
