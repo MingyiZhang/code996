f6144ad8e588ecc3fb49b94d6b96f2ec5af0bc11
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-17 22:56:49 +0200

    Add leetcode 763, java

---
+++ solutions/java/src/partitionLabels/solution.java
@@ -0,0 +1,25 @@
+package partitionLabels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] ends = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+            ends[S.charAt(i) - 'a'] = i;
+        }
+        int start = 0, end = 0;
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < S.length(); i++) {
+            if (ends[S.charAt(i) - 'a'] > end) {
+                end = ends[S.charAt(i) - 'a'];
+            } 
+            if (end == i) {
+                ret.add(end - start + 1);
+                start = i + 1;
+            } 
+        }
+        return ret;
+    }
+}
