043dcec09116e5d37de12c352d4171ed93e7bccf
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-22 23:05:51 +0200

    Add leetcode 455, java

---
+++ solutions/java/src/assignCookies/Solution.java
@@ -0,0 +1,21 @@
+package assignCookies;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = g.length - 1;
+        int j = s.length - 1;
+        int numberOfContentChildren = 0;
+        while (i >= 0 && j >= 0) {
+            if (g[i] <= s[j]) {
+                j--;
+                numberOfContentChildren++;
+            }
+            i--;
+        }
+        return numberOfContentChildren;
+    }
+}
