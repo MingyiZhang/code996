d9f15138c701996dd34cf4349e2d9ba79db2191f
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-23 16:00:16 +0200

    Fix typo

--- solutions/java/src/isGraphBipartite/Solution.java
+++ solutions/java/src/isGraphBipartite/Solution.java
@@ -1,35 +1,33 @@
 package isGraphBipartite;
 
 public class Solution {
-    class Solution {
-        public boolean isBipartite(int[][] graph) {
-            int length = graph.length;
-            if (length == 0) {
-                return false;
-            }
-            int[] color = new int[length];
-            for (int i = 0; i < graph.length; i++) {
-                if (color[i] == 0) {
-                    if (!isBipartite(graph, i, 1, color)) {
-                        return false;
-                    }
+    public boolean isBipartite(int[][] graph) {
+        int length = graph.length;
+        if (length == 0) {
+            return false;
+        }
+        int[] color = new int[length];
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == 0) {
+                if (!isBipartite(graph, i, 1, color)) {
+                    return false;
                 }
             }
-            return true;
         }
+        return true;
+    }
 
-        private boolean isBipartite(int[][] graph, int node, int currentColor, int[] color) {
-            if (color[node] == 0) {
-                color[node] = currentColor;
-                for (int nextNode : graph[node]) {
-                    if (!isBipartite(graph, nextNode, -currentColor, color)) {
-                        return false;
-                    }
+    private boolean isBipartite(int[][] graph, int node, int currentColor, int[] color) {
+        if (color[node] == 0) {
+            color[node] = currentColor;
+            for (int nextNode : graph[node]) {
+                if (!isBipartite(graph, nextNode, -currentColor, color)) {
+                    return false;
                 }
-            } else if (color[node] != currentColor) {
-                return false;
             }
-            return true;
+        } else if (color[node] != currentColor) {
+            return false;
         }
+        return true;
     }
 }
