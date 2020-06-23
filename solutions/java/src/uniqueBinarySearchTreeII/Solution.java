b8afe7c6dbe1c63b1741c3e276967ab0f98a9d38
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-23 23:25:39 +0200

    Update leetcode 95, java, recursive solution

--- solutions/java/src/uniqueBinarySearchTreeII/Solution.java
+++ solutions/java/src/uniqueBinarySearchTreeII/Solution.java
@@ -2,41 +2,68 @@ package uniqueBinarySearchTreeII;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Solution {
+    // recursive
     public List<TreeNode> generateTrees(int n) {
         if (n == 0) {
             return new ArrayList<TreeNode>();
         }
-        Map<String, List<TreeNode>> intervalToTreeNodeList = new HashMap<>();
-        return generateTreesImpl(1, n, intervalToTreeNodeList); 
+        return generateTreesImpl(1, n);
     }
-
-    private List<TreeNode> generateTreesImpl(int left, int right, Map<String, List<TreeNode>> intervalToTreeNodeList) {
+    
+    private List<TreeNode> generateTreesImpl(int left, int right) {
         if (left > right) {
             return Collections.singletonList(null);
         }
-
-        String intervalKey = String.format("%d_%d", left, right);
-        if (intervalToTreeNodeList.containsKey(intervalKey)) {
-            return intervalToTreeNodeList.get(intervalKey);
-        }
-
-        List<TreeNode> treeNodeList = new ArrayList<>();
+        
+        List<TreeNode> TreeNodeList = new ArrayList<>();
         for (int i = left; i <= right; i++) {
-            List<TreeNode> leftTreeNodesList = generateTreesImpl(left, i - 1, intervalToTreeNodeList);
-            List<TreeNode> rightTreeNodesList = generateTreesImpl(i + 1, right, intervalToTreeNodeList);
-            for (TreeNode leftNode : leftTreeNodesList) {
-                for (TreeNode rightNode : rightTreeNodesList) {
-                    treeNodeList.add(new TreeNode(i, leftNode, rightNode));
+            List<TreeNode> leftTreeNodeList = generateTreesImpl(left, i - 1);
+            List<TreeNode> rightTreeNodeList = generateTreesImpl(i + 1, right);
+            for (TreeNode leftNode : leftTreeNodeList) {
+                for (TreeNode rightNode : rightTreeNodeList) {
+                    TreeNode head = new TreeNode(i, leftNode, rightNode);
+                    TreeNodeList.add(head);
                 }
             }
         }
-        intervalToTreeNodeList.put(intervalKey, treeNodeList);
-        return treeNodeList;
+        return TreeNodeList;
     }
+
+    // Dynamic programming
+
+    // public List<TreeNode> generateTrees(int n) {
+    //     if (n == 0) {
+    //         return new ArrayList<TreeNode>();
+    //     }
+    //     Map<String, List<TreeNode>> intervalToTreeNodeList = new HashMap<>();
+    //     return generateTreesImpl(1, n, intervalToTreeNodeList); 
+    // }
+
+    // private List<TreeNode> generateTreesImpl(int left, int right, Map<String, List<TreeNode>> intervalToTreeNodeList) {
+    //     if (left > right) {
+    //         return Collections.singletonList(null);
+    //     }
+
+    //     String intervalKey = String.format("%d_%d", left, right);
+    //     if (intervalToTreeNodeList.containsKey(intervalKey)) {
+    //         return intervalToTreeNodeList.get(intervalKey);
+    //     }
+
+    //     List<TreeNode> treeNodeList = new ArrayList<>();
+    //     for (int i = left; i <= right; i++) {
+    //         List<TreeNode> leftTreeNodesList = generateTreesImpl(left, i - 1, intervalToTreeNodeList);
+    //         List<TreeNode> rightTreeNodesList = generateTreesImpl(i + 1, right, intervalToTreeNodeList);
+    //         for (TreeNode leftNode : leftTreeNodesList) {
+    //             for (TreeNode rightNode : rightTreeNodesList) {
+    //                 treeNodeList.add(new TreeNode(i, leftNode, rightNode));
+    //             }
+    //         }
+    //     }
+    //     intervalToTreeNodeList.put(intervalKey, treeNodeList);
+    //     return treeNodeList;
+    // }
     
 }
