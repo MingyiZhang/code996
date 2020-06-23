f7bae289f68084ffd5415678c3e7dd6148f18cef
Mingyi Zhang <mingyizhang1987@gmail.com>
2020-06-23 22:38:23 +0200

    Add leetcode 744, java

---
+++ solutions/java/src/findSmallestLetterGreaterThanTarget/Solution.java
@@ -0,0 +1,20 @@
+package findSmallestLetterGreaterThanTarget;
+
+public class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if (letters[right] <= target) {
+            return letters[0];
+        } else {
+            return letters[right];
+        }
+    }
+}
